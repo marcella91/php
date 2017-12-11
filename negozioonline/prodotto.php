36afa490364fba0545bd57be325e748b1713a581
marcella91 <33283772+marcella91@users.noreply.github.com>
2017-12-11 15:17:56 +0100

    Add files via upload

---
+++ negozioonline/prodotto.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('head.php'); ?>
+</head>
+<body>
+	<div class="container">
+		<div class="row riga-1">
+			<div class="col-md-12">
+				<?php include('navbar.php'); ?>
+			</div>
+		</div>
+		<div class="row riga-2">
+			<div class="col-md-4">
+				<?php include('sidebar.php'); ?>
+			</div>
+			<div class="col-md-8">
+				
+			</div>
+			
+		</div>
+		<div class="riga-3">
+			<div class="col-md-12">
+				footer
+			</div>
+			
+		</div>
+	</div>
+
+</body>
+</html>
